6ae2caeee3a634476732844b04a6bf5c4b714a5d
Benjamin Acevedo <benjaminacevedo@Benjamins-MacBook-Pro-2.local>
2017-04-21 12:29:22 -0700

    Adjusted nav bar for user pathing for show page. Adjusted migration for friendships. Adjusted friends controllers.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,25 +1,28 @@
-def index
-  @friends = current_user.friends
-end
+class FriendsController < ApplicationController
 
-def new
-    @request = Friendsrequest.new
+  def index
+    @friends = current_user.friends
   end
 
-def create
-  @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-  if @friendship.save
-    flash[:notice] = "Added friend."
-    redirect_to root_url
-  else
-    flash[:error] = "Unable to add friend."
-    redirect_to root_url
-  end
-end
+  # def new
+  #     @request = Friendsrequest.new
+  # end
 
-def destroy
-  @friendship = current_user.friendships.find(params[:id])
-  @friendship.destroy
-  flash[:notice] = "Removed friendship."
-  redirect_to current_user
+  def create
+    @friendship = current_user.friendships.build(:friend_id => params[:user_id])
+    if @friendship.save
+      flash[:notice] = "Added friend."
+      redirect_to root_url
+    else
+      flash[:error] = "Unable to add friend."
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
+    flash[:notice] = "Removed friendship."
+    redirect_to current_user
+  end
 end
