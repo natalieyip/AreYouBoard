cb23aa32e350f82e07c1921267c71b5865602b47
Daniel Welsh <danielwelsh@borealproductions.com>
2017-04-20 22:37:13 -0700

    Add game_id and user_id to allow a game to be created. And validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,19 @@
 class ReviewsController < ApplicationController
   def create
+    p params
     @review = Review.new(review_params)
-    if @review
-
+    @review.game_id = params[:game_id]
+    @review.user = current_user
+    if @review.save
+      redirect_to games_path
     else
-
+      # This must be changed
+      redirect_to games_path
     end
   end
 
   private
   def review_params
-    params.require(:new_review).permit(:title, :body)
+    params.require(:review).permit(:title, :body, :game)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :game
+  validates :user, :game, :body, :title, presence: true
 end
