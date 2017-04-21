aa847fb8156abd242dc55933156c59907f0a9d98
Natalie Yip <yip.natalie92@gmail.com>
2017-04-21 11:43:27 -0700

    Implementing votes to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,6 +12,34 @@ class ReviewsController < ApplicationController
     end
   end
 
+  # def upvote
+  #   @review = Game.find(params[:game_id])
+  #   @vote = Vote.find_by(voteable: @game, user_id: current_user.id)
+  #   if @vote
+  #     @vote.increase
+  #     @vote.save
+  #     redirect_to @game
+  #   else
+  #     @vote = Vote.new(voteable: @game, user_id: current_user.id, value: 1 )
+  #     @vote.save
+  #     redirect_to @game
+  #   end
+  # end
+
+  # def downvote
+  #   @game = Game.find(params[:game_id])
+  #   @vote = Vote.find_by(voteable: @game, user_id: current_user.id)
+  #   if @vote
+  #     @vote.decrease
+  #     @vote.save
+  #     redirect_to @game
+  #   else
+  #     @vote = Vote.new(voteable: @game, user_id: current_user.id, value: 1 )
+  #     @vote.save
+  #     redirect_to @game
+  #   end
+  # end
+
   private
   def review_params
     params.require(:review).permit(:title, :body, :game)
