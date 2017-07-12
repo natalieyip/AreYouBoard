91b9369020ada0e773533f46dc42d3e7babb746f
Natalie Yip <yip.natalie92@gmail.com>
2017-07-11 18:44:31 -0700

    incorporate paperclip

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,6 +7,9 @@ class Game < ApplicationRecord
   has_many :users, through: :game_users
 
   validates :title, :players, :age, :play_time, :complexity, :publisher, :description, presence: true
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://www.clker.com/cliparts/b/S/4/T/8/W/image-missing-md.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates :avatar, attachment_presence: true
 
   def game_in_collection?(user)
     GameUser.where(game: self, user: user).length > 0
